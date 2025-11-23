6bd63e3320a263d28a06705606095cdbf56b3c12
YooooSys <apk11pbc@gmail.com>
2025-11-23 23:04:34 +0700

    fix errors

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,14 @@
-return {
-  "mason-org/mason.nvim",
-  dependencies = { "williamboman/mason-lspconfig.nvim" },
-  opts = {},  -- Mason default options
-  config = function()
-    -- Setup Mason
-
-    -- Setup Mason-LSPconfig to ensure OmniSharp is installed
-    require("mason").setup({
-      ensure_installed = { "omnisharp" },
-      automatic_installation = true, -- optional, auto install LSPs if missing
-    })
-  end,
-}
+return {
+  "mason-org/mason.nvim",
+  dependencies = { "williamboman/mason-lspconfig.nvim" },
+  opts = {},  -- Mason default options
+  config = function()
+    -- Setup Mason
+
+    -- Setup Mason-LSPconfig to ensure OmniSharp is installed
+    require("mason").setup({
+      ensure_installed = { "omnisharp" },
+      automatic_installation = true, -- optional, auto install LSPs if missing
+    })
+  end,
+}
